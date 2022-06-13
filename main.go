ac7ae0b1bcb4ff8d02b11cc8cdd419ce7060f096
Abubakar Abdullahi <39764887+GenerousDev@users.noreply.github.com>
2022-06-13 20:33:09 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"Shopify-Challenge/configs"
+	"Shopify-Challenge/routes"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
+
+func main() {
+
+	fmt.Println("Entry Poin")
+
+	router := gin.Default()
+	configs.ConnectDB()
+
+	//routes
+	routes.ItemRoute(router)
+	router.Use(CORSMiddleware())
+	port := os.Getenv("PORT")
+
+	router.Run(":" + port)
+}
